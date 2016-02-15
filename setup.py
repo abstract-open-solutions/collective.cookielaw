ce0317c1da541be6017f8e78ca1d3893009c6f3b
Giorgio Borelli <giorgio.borelli@abstract.it>
2016-02-15 11:15:12 +0100

    Back to development: 1.1

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ long_description = (
 
 setup(
     name='collective.cookielaw',
-    version='1.0',
+    version='1.1.dev0',
     description="Make your Plone site compliant to EU cookie law",
     long_description=long_description,
     # Get more from http://pypi.python.org/pypi?%3Aaction=list_classifiers
